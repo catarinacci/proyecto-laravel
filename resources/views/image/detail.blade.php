cfb2abf44e44c6848c939e8544764f431068cb11
Gabriel Catarinacci <catarinacci@gmail.com>
2020-09-18 20:24:49 -0300

    Video desde el 377 hasta el 381

--- resources/views/image/detail.blade.php
+++ resources/views/image/detail.blade.php
@@ -3,10 +3,10 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
             @include('includes.message')
-            @foreach($images as $image)
-            <div class="card pub_image">
+            
+            <div class="card pub_image pub_image_detail">
                 <div class="card-header">
                     @if($image->user->image)
                     <div class="container-avatar">
@@ -14,35 +14,39 @@
                     </div>
                     @endif
                     <div class="data-user">
-                        <a href="{{route('image.detail', ['id' => $image->id])}}">
                         {{$image->user->name.' '.$image->user->surname}}
                         <span class="nickname">{{' | @'.$image->user->nick}}</span>
-                        </a>
                     </div>
                 </div>
                 <div class="card-body">
-                    <div class="image-container image-detail">
+                    <div class="image-container">
                         <img src="{{route('image.file', ['filename'=>$image->image_path])}}" alt="">
                     </div>                   
                     <div class="description">
                         <span class="nickname">{{'@'.$image->user->nick}}</span>
-                        <span class="nickname date"> {{' | '.\FormatTime::LongTimeFilter($image->created_at)}} </span>
                         <p>{{$image->description}}</p>
                     </div>
                     <div class="likes">
                         <img src="{{asset('img/heart-black.png')}}"  alt="">
                     </div>
+                    <div class='clearfix'></div>
                     <div class="comments">
-                        <a href="" class="btn btn-sm btn-warning btn-comments">
-                            Comentarios ({{count($image->comments)}})
-                        </a>
+                       <h2>Comentarios ({{count($image->comments)}})</h2>
+                       <hr>
+                       <form method="POST" action="">
+                           <input type="hidden" name="image_id" value="{{$image->id}}">
+                           <p>
+                               <textarea class="form-control" name="content" required id="" cols="30" rows="10"></textarea>
+                               <hr>
+                               <button type="submit" class="btn btn-success">
+                                   Enviar
+                               </button>
+                           </p>
+                       </form>
                     </div>
                 </div>
             </div>
-            @endforeach
-            <!-- Enlaces de paginación -->
-            <div class="clearfix"></div>
-            {{$images->links()}}
+            
         </div>
     </div>
 </div>

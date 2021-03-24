06b2d5bf0e1b27c4a1853f9f5d0e3a981a7cf58a
Gabriel Catarinacci <catarinacci@gmail.com>
2021-03-24 19:57:39 -0300

    aplicando laravel mix

--- public/js/main.js
+++ public/js/main.js
@@ -1,71 +1,166 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./resources/js/main.js":
+/*!******************************!*\
+  !*** ./resources/js/main.js ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
 var url = 'http://proyecto-laravel.com.devel/';
+window.addEventListener("load", function () {
+  $('.btn-like').css('cursor', 'pointer');
+  $('.btn-dislike').css('cursor', 'pointer'); //Botón de Like
 
-window.addEventListener("load", function(){
-
-    $('.btn-like').css('cursor', 'pointer');
-    $('.btn-dislike').css('cursor', 'pointer');
-
-    //Botón de Like
-    function like(){
-        
-        $('.btn-like').unbind('click').click(function(){
-            console.log('like');
-            $(this).addClass('btn-dislike').removeClass('btn-like');
-            $(this).attr('src', url+'img/heart-red.png');
-
-            $.ajax({
-                url: url+'/like/'+$(this).data('id'),
-                type:'GET',
-
-                success: function(response){
-                    if(response.like){
-                        console.log(response.like);
-                        console.log('Has dado like a la publicación');
-                    }else{
-                        console.log('Error al dar like');
-                    }
-                    
-                }
-            });
-
-            dislike();
-        });
-    }
-    like();
-    
-
-    //Botón de Dislike
-    function dislike(){
-        
-        $('.btn-dislike').unbind('click').click(function(){
-            console.log('dislike');
-            $(this).addClass('btn-like').removeClass('btn-dislike');
-            $(this).attr('src', url+'img/heart-black.png');
-
-            $.ajax({
-                url: url+'/dislike/'+$(this).data('id'),
-                type:'GET',
-
-                success: function(response){
-                    if(response.like){
-                        console.log(response.like);
-                        console.log('Has dado dislike a la publicación');
-                    }else{
-                        console.log('Error al dar dislike');
-                    }
-                    
-                }
-            });
-            like();
-        });
-    }
-    dislike();
-
-    //BUSCADOR
-    $('#buscador').submit(function(){
-        
-        $(this).attr('action',url+'gente/'+$('#buscador #search').val());
-        
+  function like() {
+    $('.btn-like').unbind('click').click(function () {
+      console.log('like');
+      $(this).addClass('btn-dislike').removeClass('btn-like');
+      $(this).attr('src', url + 'img/heart-red.png');
+      $.ajax({
+        url: url + '/like/' + $(this).data('id'),
+        type: 'GET',
+        success: function success(response) {
+          if (response.like) {
+            console.log(response.like);
+            console.log('Has dado like a la publicación');
+          } else {
+            console.log('Error al dar like');
+          }
+        }
+      });
+      dislike();
     });
-    
+  }
+
+  like(); //Botón de Dislike
+
+  function dislike() {
+    $('.btn-dislike').unbind('click').click(function () {
+      console.log('dislike');
+      $(this).addClass('btn-like').removeClass('btn-dislike');
+      $(this).attr('src', url + 'img/heart-black.png');
+      $.ajax({
+        url: url + '/dislike/' + $(this).data('id'),
+        type: 'GET',
+        success: function success(response) {
+          if (response.like) {
+            console.log(response.like);
+            console.log('Has dado dislike a la publicación');
+          } else {
+            console.log('Error al dar dislike');
+          }
+        }
+      });
+      like();
+    });
+  }
+
+  dislike(); //BUSCADOR
+
+  $('#buscador').submit(function () {
+    $(this).attr('action', url + 'gente/' + $('#buscador #search').val());
+  });
 });
+
+/***/ }),
+
+/***/ 1:
+/*!************************************!*\
+  !*** multi ./resources/js/main.js ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\master_php\proyecto-laravel\resources\js\main.js */"./resources/js/main.js");
+
+
+/***/ })
+
+/******/ });

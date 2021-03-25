76aa737547aff1268910f82c5cd200004f291665
Gabriel Catarinacci <catarinacci@gmail.com>
2021-03-24 23:13:06 -0300

    icono larafoto

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,6 +27,7 @@
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     <!-- {{ config('app.name', 'Laravel') }} -->
+                    <i class="fas fa-camera"></i>
                     Larafoto
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
